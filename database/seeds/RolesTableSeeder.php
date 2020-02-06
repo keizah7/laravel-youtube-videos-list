8f117a5ac9e1d65622a6d83971a0dd4ca0fb54c2
keizah7 <murashka.arturas@gmail.com>
2020-02-06 19:07:04 +0200

    Role migration & seeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'agent',
+        ]);
+
+        Role::create([
+            'name' => 'teamleader',
+        ]);
+    }
+}

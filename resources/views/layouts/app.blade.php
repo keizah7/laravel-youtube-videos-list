68efe9fe910335f10c8440b925b2c23d85f26f16
keizah7 <murashka.arturas@gmail.com>
2020-02-12 00:31:44 +0200

    Show messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,27 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        @if ($errors->any())
+                            <div class="alert">
+                                <ul class="list-group">
+                                    @foreach ($errors->all() as $error)
+                                        <li class="list-group-item list-group-item-danger">{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        @if(session()->has('message'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session()->get('message') }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
             @yield('content')
         </main>
     </div>
